fe3ce127ad325bad716ae6f0ca90124840f8d0ec
Joel Raju <joelraju@ymail.com>
2018-01-10 09:45:48 +0530

    added Search component added styled-components dependency

---
+++ src/components/Search.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {inputValue: ''};
+
+		this.onSearchTermChanged = this.onSearchTermChanged.bind(this);
+	}
+
+	componentDidMount() {
+
+	}
+
+	componentWillReceiveProps() {
+
+	}
+
+	// shouldComponentUpdate() {
+
+	// }
+
+	onSearchTermChanged(event) {
+		this.setState({inputValue: event.target.value});
+	}
+
+	render() {
+		const { inputValue } = this.state;
+		return (<div>
+			<input 
+				type="text"
+				value={inputValue}
+				onChange={this.onSearchTermChanged} />
+			</div>	
+		);
+	}
+}
+
+export default Search;
